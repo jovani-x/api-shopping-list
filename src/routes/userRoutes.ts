c2f87f0bc71af66d7a11aafb2c126b6dccfe4edb
jovani-x <jovani-x@users.noreply.github.com>
2024-05-27 13:21:34 +0200

    clean user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,15 +2,14 @@ import userController from "../controllers/userController.js";
 import express from "express";
 
 const router = express.Router();
-const apiAuthUrl = "";
 
 // login
-router.post(`${apiAuthUrl}/login`, userController.signin);
+router.post("/login", userController.signin);
 // logout
-router.post(`${apiAuthUrl}/logout`, userController.signout);
+router.post("/logout", userController.signout);
 // register
-router.post(`${apiAuthUrl}/register`, userController.signup);
+router.post("/register", userController.signup);
 // forget
-router.post(`${apiAuthUrl}/forget`, userController.forget);
+router.post("/forget", userController.forget);
 
 export default router;
